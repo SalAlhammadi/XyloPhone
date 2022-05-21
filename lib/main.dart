557ff9a4edd84cffb4f6799d515d01eec8da2d2c
Alhammadi Salem <48154770+SalAlhammadi@users.noreply.github.com>
2022-05-21 20:13:02 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNo){
+    final player = AudioCache();
+    player.play('note$noteNo.wav');
+}
+
+FlatButton buildKey(int no){
+    var j = 700;
+    if (no%2==0) j = 900; else j = 700;
+  return FlatButton(
+    color: Colors.blueGrey[j],
+    onPressed:(){
+      playSound(no);
+    },
+  );
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black26,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            for (int i = 1 ; i < 8 ; i++)
+              Expanded(child: buildKey(i))
+            ],
+          ),
         ),
       ),
     );
